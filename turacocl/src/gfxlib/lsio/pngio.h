8b7b68c0602749e5fed4fd6387f58f6944b72b34
yorgle <yorgle@71a33532-c446-0410-90bf-89e31ca364bc>
2009-11-06 15:31:57 +0000

    Renamed so as to not conflict with libpng

---
+++ turacocl/src/gfxlib/lsio/pngio.h
@@ -0,0 +1,24 @@
+/*
+ *  PNGio
+ *
+ *   simple save/loader for png data
+ *
+ * $Id: PNG.h,v 1.6 2003/04/08 14:57:44 jerry Exp $
+ *
+ * $Log: PNG.h,v $
+ */
+
+#include "image.h"
+#include "ls.h"		/* for "FileFormat" */
+
+
+#ifndef __PNGIO_H__
+#define __PNGIO_H__
+
+IMAGE * PNG_Load( char * filename );
+void    PNG_Save( char * filename, IMAGE * tosave );
+FileFormat PNG_SupportedFile( char * filename );
+
+int PNG_Query( void );
+
+#endif
